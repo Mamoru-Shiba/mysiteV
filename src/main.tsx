835a778608e70bfaf5d5b13359a00a62f0f48d43
mamoru <maruru00551199@gmail.com>
2021-08-13 16:38:49 +0900

    CSS色付け成功

--- src/main.tsx
+++ src/main.tsx
@@ -3,23 +3,20 @@ import * as ReactDOM from 'react-dom';
 import styled from '@emotion/styled';
 import { SubComponent } from './sub-component';
 
-const div = styled.h1`
+const Header = styled.h1`
   background-color: hotpink;
   font-size: 24px;
   border-radius: 4px;
   color: black;
   font-weight: bold;
 `
-
-// const Pin = css({
-//   color: 'green',
-// });
+// const Test = styled.
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello React!</h1>
+        <Header>Hello React!</Header>
         <SubComponent name="My Counter for TypeScript" />
       </div>
     );
